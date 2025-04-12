26358e7bae7384bbcd1cf1055f86a12e739ea032
prowang01 <prosper082003@outlook.com>
2025-04-12 20:23:06 +0200

    Optimize transfer_style with image resizing to reduce memory usage in Colab

--- api/transfer_style.py
+++ api/transfer_style.py
@@ -1,35 +1,37 @@
-import matplotlib.pylab as plt
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 
-def transfer_style(content_image, style_image, model_path):
+def transfer_style(content_image_path, style_image_path, model_path, max_dim=512):
     """
-    :param content_image: path of the content image
-    :param style_image: path of the style image
-    :param model_path: path to the downloaded pre-trained model.
+    Apply neural style transfer on two images with optional resizing to save memory.
+
+    :param content_image_path: path to the content image
+    :param style_image_path: path to the style image
+    :param model_path: path to the downloaded pre-trained model (TF Hub)
+    :param max_dim: max dimension to resize content image (default 512)
     :return: stylized image as a 3D numpy array
     """
+    from PIL import Image
 
-    print("Loading images...")
-    content_image = plt.imread(content_image)
-    style_image = plt.imread(style_image)
+    def load_and_process_image(path, resize=True):
+        img = Image.open(path).convert('RGB')
+        if resize:
+            img.thumbnail((max_dim, max_dim))
+        img = np.array(img).astype(np.float32)[np.newaxis, ...] / 255.
+        return tf.constant(img)
 
-    print("Resizing and Normalizing images...")
-    content_image = content_image.astype(np.float32)[np.newaxis, ...] / 255.
-    style_image = style_image.astype(np.float32)[np.newaxis, ...] / 255.
+    print("Loading and resizing images...")
+    content_image = load_and_process_image(content_image_path, resize=True)
+    style_image = load_and_process_image(style_image_path, resize=True)
     style_image = tf.image.resize(style_image, (256, 256))
 
     print("Loading pre-trained model...")
     hub_module = hub.load(model_path)
 
     print("Generating stylized image now...")
-    outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
+    outputs = hub_module(content_image, style_image)
     stylized_image = outputs[0]
 
-    stylized_image = np.array(stylized_image)
-    stylized_image = stylized_image.reshape(
-        stylized_image.shape[1], stylized_image.shape[2], stylized_image.shape[3])
-
     print("Stylizing completed.")
-    return stylized_image
+    return stylized_image[0].numpy()  # Remove batch dimension
